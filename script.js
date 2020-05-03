60837968ec1d58823c2f6eeb687f07f08ca0a293
yevamelikyan <62141135+yevamelikyan@users.noreply.github.com>
2020-05-03 19:17:34 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function getRndInteger(min, max) {
+function getRndInteger (min, max) {
    var spotNumber = Math.floor(Math.random() * (max - min)) + min;
    document.getElementById('getRndInteger').innerHTML = spotNumber;
+}  
+ 
+function sendInput() {
+  var name = document.getElementById('name').value;
+  var surname = document.getElementById('surname').value;
+  if (name == "" || surname == "") {
+  alert("Please Fill in Your Name"); 
+  }
+  else { 
+  sessionStorage.setItem('name', name);
+  sessionStorage.setItem('surname', surname);
+  window.location = 'app.html'; 
+  }
 }
-
+  
